2a887bc7de2971f4785399ce317389571558c46a
Basile <basile.graf@a3.epfl.ch>
2024-10-19 15:13:45 +0200

    First commit

---
+++ buddhist-mindmap.mm
@@ -0,0 +1,203 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Buddhism" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1729341763577" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Four Noble Thruths" POSITION="right" ID="ID_683459160" CREATED="1729336173896" MODIFIED="1729336220067">
+<edge COLOR="#ff0000"/>
+<node TEXT="Suffering" ID="ID_620406646" CREATED="1729336266000" MODIFIED="1729338760497"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Suffering is defined as the clinging to any of the five aggregates.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="" ID="ID_1121986458" CREATED="1729336958801" MODIFIED="1729336958801">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Clinging to Form (Rupa)" ID="ID_1481988263" CREATED="1729336706875" MODIFIED="1729336845949"/>
+<node TEXT="Clinging to Sensation(Vedana)" ID="ID_727164738" CREATED="1729336829423" MODIFIED="1729336865574"/>
+<node TEXT="Clinging to Percetion (Sanna)" ID="ID_1526206893" CREATED="1729336866127" MODIFIED="1729336883594"/>
+<node TEXT="Clinging to formation (Sankara)" ID="ID_821013727" CREATED="1729336884287" MODIFIED="1729336899224"/>
+<node TEXT="Clinging to consciousness (Vinnana)" ID="ID_925544304" CREATED="1729336899662" MODIFIED="1729336946739"/>
+<node TEXT="" ID="ID_636255038" CREATED="1729336958797" MODIFIED="1729336958800">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="5 aggregates" ID="ID_307079737" CREATED="1729336958802" MODIFIED="1729336974543"/>
+</node>
+</node>
+<node TEXT="Cause of Suffering" ID="ID_912068313" CREATED="1729336273200" MODIFIED="1729341565269"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Describes how any form of craving causes or leads to further becoming, in turn leading to more suffering. There are three types of craving. All lead to further becoming, including craving for non-becoming.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Craving for sensuality" ID="ID_600786780" CREATED="1729338985071" MODIFIED="1729339035084"/>
+<node TEXT="Craving for becoming" ID="ID_255927018" CREATED="1729338995966" MODIFIED="1729339078781"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Becoming: the assumption of an identity in a particular world of experience.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="craving for non-becoming" ID="ID_21338972" CREATED="1729339006718" MODIFIED="1729339016469"/>
+</node>
+<node TEXT="Existence of a Path" ID="ID_41577358" CREATED="1729336284929" MODIFIED="1729336691471"/>
+<node TEXT="Practice of the Path" ID="ID_1024635940" CREATED="1729336691971" MODIFIED="1729336704957"/>
+</node>
+<node TEXT="Noble Eightfold Path" POSITION="right" ID="ID_1912295117" CREATED="1729336197518" MODIFIED="1729336232375">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Three Characteristics" POSITION="right" ID="ID_688640389" CREATED="1729336197662" MODIFIED="1729336240078">
+<edge COLOR="#00ff00"/>
+<node TEXT="Dukka" ID="ID_502616611" CREATED="1729342604561" MODIFIED="1729342647556"/>
+<node TEXT="Anicca" ID="ID_641021274" CREATED="1729342620129" MODIFIED="1729342626906"/>
+<node TEXT="Anata" ID="ID_25108434" CREATED="1729342627633" MODIFIED="1729342640964"/>
+</node>
+<node TEXT="Teaching peculiarities" POSITION="right" ID="ID_250237987" CREATED="1729341769990" MODIFIED="1729341796556">
+<edge COLOR="#7c0000"/>
+<node TEXT="Four types of clinging to be relinquished" ID="ID_143631328" CREATED="1729341803861" MODIFIED="1729342120938"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The teaching of the Buddha has that four types of clinging must be let go of. The fourth one (clinging to doctrines of the self) is said to be unique to the teaching of the Buddha.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Sensuality" ID="ID_1304184598" CREATED="1729341909920" MODIFIED="1729341932820"/>
+<node TEXT="Views" ID="ID_795174529" CREATED="1729341933353" MODIFIED="1729341939177"/>
+<node TEXT="Habits &amp; rituals" ID="ID_1420368508" CREATED="1729341939499" MODIFIED="1729341946010"/>
+<node TEXT="Doctrines of the self" ID="ID_629858196" CREATED="1729341946510" MODIFIED="1729341965317"/>
+</node>
+<node TEXT="Knowledge / comprehension" ID="ID_841744779" CREATED="1729342168629" MODIFIED="1729342235597"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      What does it mean for something to be known / understood / comprehended?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Paticca Samuppada" ID="ID_1999741121" CREATED="1729342304435" MODIFIED="1729342575679"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Seeing experience in terms of dependent origination (paticca samuppada). In turn, this is knowledge w.r.t. the four noble truths.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Ending" ID="ID_426155063" CREATED="1729342398292" MODIFIED="1729342487566"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Seeing/understanding in a way that ends passion, aversion &amp; delusion for what is comprehended
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Paticca Samuppada" POSITION="right" ID="ID_1962756449" CREATED="1729342757167" MODIFIED="1729342767546">
+<edge COLOR="#7c007c"/>
+</node>
+</node>
+</map>
